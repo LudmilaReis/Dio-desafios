596dd6f92380d2cf8f7e515e4aa51236545981d3
Ludmila Reis <ludmila.creiis@gmail.com>
2021-09-24 11:56:29 -0300

    Inclusão de novos códigos

---
+++ Csharp/Diferença.cs
@@ -0,0 +1,27 @@
+/* Leia quatro valores inteiros A, B, C e D. A seguir, calcule e mostre a diferença do produto de A e B pelo produto de C e D segundo a fórmula: DIFERENCA = (A * B - C * D).
+
+Entrada
+O arquivo de entrada contém 4 valores inteiros.
+
+Saída
+Imprima a mensagem DIFERENCA com todas as letras maiúsculas, conforme exemplo abaixo, com um espaço em branco antes e depois da igualdade. */
+using System;
+
+namespace Desafio
+{
+    class Diferenca
+    {
+        static void Main(string[] args)
+        {
+          
+          int a,b,c,d;
+          a = int.Parse(Console.ReadLine());
+          b = int.Parse(Console.ReadLine());
+          c = int.Parse(Console.ReadLine());
+          d = int.Parse(Console.ReadLine());
+       
+            Console.WriteLine("DIFERENCA = " +   ( a  * b - c * d ));
+            Console.ReadKey();
+        }
+    }
+}
